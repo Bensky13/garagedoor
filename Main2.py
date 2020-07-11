e463981b763f6ef0589762fd1c09ab2b90ad2c80
Bensky13 <bensky13@gmail.com>
2020-07-11 21:40:28 +0000

    wow look

--- Main2.py
+++ Main2.py
@@ -4,6 +4,11 @@ import time
 import atexit
 import threading
 import collections
+import clicksend_client
+from clicksend_client import SmsMessage
+from clicksend_client.rest import ApiException
+
+
 
 class GarageDoor:
     # Number of status entries to keep
@@ -28,6 +33,9 @@ class GarageDoor:
     #How many instances the beam is broken (door open)
     brokenCount = 0
 
+    #Number of texts sent
+    smsCounter = 0
+
     scriptRunning = True
 
     def __init__(self):
@@ -38,10 +46,37 @@ class GarageDoor:
         
         atexit.register(self.exitHandler)
 
+        config = configparser.ConfigParser()
+        config.read('settings.conf')
+
+        # Configure HTTP basic authorization: BasicAuth
+        username = config['Notifications']['clicksendAPI_username']
+        password = config['Notifications']['clicksendAPI_password']
+        notificationNumber = config['Notifications']['phonenumber']
+        configuration = clicksend_client.Configuration()
+        configuration.username = username
+        configuration.password = password
+
+        # create an instance of the API class
+        self.api_instance = clicksend_client.SMSApi(clicksend_client.ApiClient(configuration))
+
+        # If you want to explictly set from, add the key _from to the message.
+        sms_message = SmsMessage(source="python",
+                        body="Hey, the garage door has been open for five minutes.",
+                        to=notificationNumber)
+
+        self.sms_messages = clicksend_client.SmsMessageCollection(messages=[sms_message])
+
+
         garageDoorStatusThread = threading.Thread(target=self.doorStatusLoop)
+        sendSMSThread = threading.Thread(target=self.sendSMS)
+
 
         garageDoorStatusThread.start()
         garageDoorStatusThread.join()
+
+        sendSMSThread.start()
+        sendSMSThread.join()
 
     def exitHandler(self):
         print("Got an exit signal, cleaning up...")
@@ -68,6 +103,21 @@ class GarageDoor:
             else:
                 self.currentlyOpen = False
 
+    def sendSMS(self):
+        while self.scriptRunning:
+            if self.currentlyOpen == True:
+                if self.smsCounter <= 5:
+                    print("I'm gonna send a text")
+                    try:
+                        api_response = self.api_instance.sms_send_post(self.sms_messages)
+                        print(api_response)
+                        self.smsCounter += 1
+                    except ApiException as e:
+                        print("Exception when calling SMSApi->sms_send_post: %s\n" % e)
+                        self.smsCounter -=1
+                    time.sleep(300)
+            else:
+                self.smsCounter = 0
 
 if __name__ == "__main__":
     garageDoorObject = GarageDoor()
