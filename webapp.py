9b300310f25f5f1a7906ae50e804e95b648e62a4
Your Name <you@example.com>
2020-07-11 21:05:25 -0400

    Fixed webapp

--- webapp.py
+++ webapp.py
@@ -4,10 +4,12 @@ from Main2 import GarageDoor
 app = Flask(__name__)
 
 door = GarageDoor()
-
 @app.route('/')
 def doorStatus():
     if door.currentlyOpen:
         return "Garage door is: Open"
     else:
         return "Garage door is: Closed"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080")
